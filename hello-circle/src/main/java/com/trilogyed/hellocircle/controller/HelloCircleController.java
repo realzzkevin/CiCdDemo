16fdbf85950368478b9b7a77813183762a435207
zzkevin <realzzkevin@gmail.com>
2022-11-02 16:33:57 -0400

    fix answer route

--- hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
@@ -9,4 +9,8 @@ public class HelloCircleController {
     public String helloCircle(){
         return "Hello, Circle!";
     }
+    @GetMapping(value = "/answer")
+    public String getAnswer(){
+        return "The answer is 42!";
+    }
 }
